91f550b0c6d74518b1cc3531213294cb2051007e
Durrant <durrantbilankulu@gmail.com>
2021-02-16 00:50:56 +0200

    added numbers to time converter

---
+++ number-to-time.py
@@ -0,0 +1,16 @@
+# A function that converts any number into hours and minutes
+
+def number_to_time(number):
+    hours=number//60
+    minutes=number%60
+    if hours>=2:
+        if minutes>=2:
+            return "%d hours, %d minutes"%(hours,minutes)
+        else:
+            return "%d hours, %d minute"%(hours,minutes)      
+    else:
+        if hours<2 and minutes<2:
+            return "%d hour, %d minute"%(hours,minutes)
+        else:
+            return "%d hour, %d minutes"%(hours,minutes)
+            
