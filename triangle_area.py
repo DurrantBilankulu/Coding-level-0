c3487a32510eae8484280939cadcb561f0f4b0a5
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 20:12:22 +0200

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -1,6 +1,10 @@
-
 def triangle_area(side1, side2, side3):
-    semiperimeter = 0.5*(side1 + side2 + side3)
-    area = (semiperimeter * (semiperimeter-side1) * (semiperimeter-side2)
-            * (semiperimeter-side3))**0.5
+
+    semiperimeter = 0.5 * (side1 + side2 + side3)
+    area = (
+        semiperimeter
+        * (semiperimeter - side1)
+        * (semiperimeter - side2)
+        * (semiperimeter - side3)
+    ) ** 0.5
     return area
