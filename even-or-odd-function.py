a3a6045527f79f6c9201f31f58b161c64bf81631
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 09:36:11 +0200

    Update and rename even-or-odd-function.py to even_odd.py
    removed unnecessary comments top of code,  code follows pep 8 guidelines, used snake convention and followed task instructions

--- even-or-odd-function.py
+++
@@ -1,8 +0,0 @@
-#The function takes an integer as an argument, prints whether the input is odd or even
-
-def even_or_odd(integer):
-    if integer%2==0:
-        return "even"
-    else:
-        return "odd"
-    
---
+++ even_odd.py
@@ -0,0 +1,8 @@
+
+def even_odd(integer):
+    if integer % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+    
