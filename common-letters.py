a19576dcf70fe89c19b50dcc855268e8b651db05
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 09:33:22 +0200

    Update and rename common-letters.py to common_letters.py
    removed unnecessary comments top of code, variables are now meaningful, code follows pep 8 guidelines, used snake convention and followed task instructions

--- common-letters.py
+++
@@ -1,8 +0,0 @@
-#a function that takes two strings as input, and outputs the common characters/letters that they share
-
-def common_letters(string1,string2):
-    
-    common_letters= ','.join(set(string1).intersection(string2))
-    return "Common letters:%s"%common_letters
-   
-
---
+++ common_letters.py
@@ -0,0 +1,8 @@
+
+def common_letters(string1, string2):
+
+    common_letters = ','.join(set(string1).intersection(string2))
+    return "Common letters:%s" % common_letters
+
+   
+
