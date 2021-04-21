a530dd9208ab7d490fb7de03c686c763ea719180
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 20:09:15 +0200

    Update and rename vowels_inString.py to vowels_in_string.py

--- vowels_in_string.py
+++ vowels_in_string.py
@@ -1,8 +1,5 @@
-
-
-def vowels_inString(string):
+def vowels_in_string(string):
 
     all_vowels = "AaEeIiOoUu"
     list_vowels = [vowel.lower() for vowel in string if vowel in all_vowels]
     return "Vowels: %s" % ",".join(dict.fromkeys(list_vowels))
-
