96b074eec57b6516b9182e3a8e70825c1c2aab3d
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 19:44:57 +0200

    Update common_letters.py

--- common_letters.py
+++ common_letters.py
@@ -1,8 +1,4 @@
-
 def common_letters(string1, string2):
 
-    common_letters = ','.join(set(string1).intersection(string2))
+    common_letters = ",".join(set(string1).intersection(string2))
     return "Common letters:%s" % common_letters
-
-   
-
