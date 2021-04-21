243a0196d8e07e399699210b5471d8fd2e6c810a
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-21 11:30:31 +0200

    Update and rename vowels-in-string.py to vowels_inString.py
    removed unnecessary comments top of code, variables are now meaningful, code follows pep 8 guidelines, used snake convention and followed task instructions

---
+++ vowels_inString.py
@@ -0,0 +1,8 @@
+
+
+def vowels_inString(string):
+
+    all_vowels = "AaEeIiOoUu"
+    list_vowels = [vowel.lower() for vowel in string if vowel in all_vowels]
+    return "Vowels: %s" % ",".join(dict.fromkeys(list_vowels))
+
