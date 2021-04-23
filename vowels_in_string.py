c6f451cac469430581580df555379bb30e3683bf
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-23 07:18:51 +0200

    Update vowels_in_string.py
    used f-string, the function now prints instead of return

--- vowels_in_string.py
+++ vowels_in_string.py
@@ -2,4 +2,5 @@ def vowels_in_string(string):
 
     all_vowels = "AaEeIiOoUu"
     list_vowels = [vowel.lower() for vowel in string if vowel in all_vowels]
-    return "Vowels: %s" % ",".join(dict.fromkeys(list_vowels))
+    vowels = ",".join(dict.fromkeys(list_vowels))
+    print(f"Vowels: {vowels}")
