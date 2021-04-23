ba66667f36f0b1f734554e0c7cf5ac5609503680
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-23 08:19:11 +0200

    Update and rename even_odd.py to even_or_odd.py
    named function appropriately, the function now "prints" instead of "return" as  per instructions

--- even_odd.py
+++
@@ -1,5 +0,0 @@
-def even_odd(integer):
-    if integer % 2 == 0:
-        return "even"
-    else:
-        return "odd"
---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+def even_or_odd(integer):
+
+    if integer % 2 == 0:
+        number_type = "even"
+    else:
+        number_type = "odd"
+    print(number_type)
