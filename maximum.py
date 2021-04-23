016512c352c6be8f063081a4db5338a60f95cf55
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-23 07:09:24 +0200

    Update and rename max_number.py to maximum.py
    now n1 is tested against n3 and max_number is changed to maximum

--- maximum.py
+++ maximum.py
@@ -1,6 +1,6 @@
-def max_number(number1, number2, number3):
+def maximum(number1, number2, number3):
 
-    if number1 >= number2 and number1 >= number2:
+    if number1 >= number2 and number1 >= number3:
         largest_number = number1
 
     elif number2 >= number1 and number2 >= number3:
