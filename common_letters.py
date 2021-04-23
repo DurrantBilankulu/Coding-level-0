940eb8d61f3a8d30b5dccff216fbb98c3ae4b4cc
DurrantBilankulu <durrantbilankulu@gmail.com>
2021-04-23 07:26:51 +0200

    Update common_letters.py
    updated to use f-string

--- common_letters.py
+++ common_letters.py
@@ -1,4 +1,4 @@
 def common_letters(string1, string2):
 
     common_letters = ",".join(set(string1).intersection(string2))
-    return "Common letters:%s" % common_letters
+    return f"Common letters:{common_letters}"
